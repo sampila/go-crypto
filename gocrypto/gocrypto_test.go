977dcd221e6a41730ec854846ea1d024ecb845b2
sampila <alipxsulistio@gmail.com>
2019-12-20 17:52:52 +0700

    fixing

--- gocrypto/gocrypto_test.go
+++ gocrypto/gocrypto_test.go
@@ -2,16 +2,21 @@ package gocrypto
 
 import(
   "testing"
+  "os"
   "github.com/stretchr/testify/assert"
 )
 
-func TestHashPassword(t *testing.T){
+func TestMain(m *testing.M){
+  os.Exit(m.Run())
+}
+
+func TestHashPasswordOK(t *testing.T){
   hash, err := HashPassword("mypassword")
   assert.NotNil(t,hash)
   assert.EqualValues(t, err, nil)
 }
 
-func TestComparePassword(t *testing.T){
+func TestComparePasswordInvalid(t *testing.T){
   isFalse := CheckPasswordHash("mypass", "qoieuoqwiueoiqweq2384029")
   assert.EqualValues(t, false, isFalse)
 }
